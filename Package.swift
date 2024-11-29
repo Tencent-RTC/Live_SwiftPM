aa2ff70f7d4eb4cae853cf4eb08eac133ab0b5b4
znhyo <102845801+znhyo@users.noreply.github.com>
2024-11-29 16:01:29 +0800

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,62 @@
+// swift-tools-version: 6.0.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Live_SwiftPM",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Live_SwiftPM",
+            targets: ["Live_SwiftPM"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(
+            name: "TXLiteAVSDK_Live",
+            url: "https://liteav.sdk.qcloud.com/download/spm/12.2/live/TXLiteAVSDK_Live.xcframework.zip",
+            checksum: "c9bffc4d4bfff9b02ac682434c08af896dc69fb9a0f9284b4824695dfac1ddf1"
+        ),
+        .binaryTarget(
+            name: "TXFFmpeg",
+            url: "https://liteav.sdk.qcloud.com/download/spm/12.2/live/TXFFmpeg.xcframework.zip",
+            checksum: "33b093f50b1eb2d06ce022905aa6a3f13a63531baa3fe50f120cb5ffa9a7b36e"
+        ),
+        .binaryTarget(
+            name: "TXSoundTouch",
+            url: "https://liteav.sdk.qcloud.com/download/spm/12.2/live/TXSoundTouch.xcframework.zip",
+            checksum: "787a52631475bb579b2d7c54b6ed3225b23ddfb7a8032e8c1187d57bb17c4575"
+        ),.target(
+            name: "Live_SwiftPM",
+            dependencies: [
+                .target(name: "TXLiteAVSDK_Live"),
+                .target(name: "TXFFmpeg"),
+                .target(name: "TXSoundTouch")
+            ],
+            sources: ["Live_SwiftPM.swift"],
+            linkerSettings: [
+                .linkedFramework("AVKit"),
+                .linkedFramework("GLKit"),
+                .linkedFramework("AssetsLibrary"),
+                .linkedFramework("SystemConfiguration"),
+                .linkedFramework("CoreTelephony"),
+                .linkedFramework("AVFoundation"),
+                .linkedFramework("OpenGLES"),
+                .linkedFramework("Accelerate"),
+                .linkedFramework("MetalKit"),
+                .linkedFramework("MobileCoreServices"),
+                .linkedFramework("CoreMedia"),
+                .linkedFramework("MetalPerformanceShaders"),
+                .linkedLibrary("sqlite3.0"),
+                .linkedLibrary("c++"),
+                .linkedLibrary("resolv")
+            ]
+        )
+    ]
+)
+
